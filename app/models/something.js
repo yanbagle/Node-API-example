6d24d910426ad302385866049e7532823c3008cd
yanzhang000 <circusburger63@yahoo.com>
2017-02-04 14:55:41 -0600

    adding routes

--- app/models/something.js
+++
@@ -1,9 +0,0 @@
-var exports = module.exports = {};
-
-exports.setName = function(name){
-    this.name = name;
-}
-
-exports.getName = function(){
-    return this.name;
-}
--- server.js
+++ server.js
@@ -9,32 +9,11 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
-
-var Something = require('./app/models/something.js');
-
-// test route to make sure everything is working (accessed at GET http://localhost:8080/playground/nodeAPI)
-router.get('/', function(req, res) {
-    res.json({ message: 'hello!' });   
-});
-
-router.get('/somethingYo',function(req,res){
-    Something.setName("yo");
-    res.json({message: "Name is " + Something.getName()});
-});
-
-router.route('/something').post(function(req,res){
-    Something.setName(req.body.name);
-    res.json({message: "something created, name is " + Something.getName()});
-});
-
-// more routes for our API will happen here
+var routes = require('./app/routes/routes.js');
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
-app.use('/playground/nodeAPI', router);
+app.use('/playground', routes);
 
 // START THE SERVER
 // =============================================================================
